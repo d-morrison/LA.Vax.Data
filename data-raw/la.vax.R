6a0b4ca787557853c009ed0cc226b239e45695a8
Douglas Ezra Morrison <dezramorrison@gmail.com>
2021-11-04 16:47:29 -0700

    adding vax.last

--- data-raw/la.vax.R
+++ data-raw/la.vax.R
@@ -6,3 +6,19 @@ load_all()
 la.vax = load_data()
 
 usethis::use_data(la.vax, overwrite = TRUE)
+
+vax.last = la.vax %>%
+  group_by(Community) %>%
+  filter(!is.na(`% vaccinated 12-17`)) %>%
+  slice_tail() %>%
+  ungroup() %>%
+  select(
+    Community,
+    `Population (12-17)`,
+    `Cumulative 12-17 (Dose 1)`,
+    `% vaccinated 12-17`,
+    `Date (Dose 1)`) %>%
+  rename(Date = `Date (Dose 1)`)
+
+usethis::use_data(vax.last, overwrite = TRUE)
+
